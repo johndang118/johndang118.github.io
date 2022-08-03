859c51393036d499da726355c9678dfa9bc240e7
metainmotion <103100514+metainmotion@users.noreply.github.com>
2022-08-02 17:06:04 -0700

    spreadsheet

--- spreadsheet.js
+++ spreadsheet.js
@@ -12,13 +12,20 @@ db.collection("students").get().then((querySnapshot) => {
 let displayStudents = (students) =>
 {
 
-document.getElementById('studenttable').innerHTML = "";
+//display date headers
 let studentdata = "";
+
+let sundayDates = getSundays();
+sundayDates.forEach((sunday) => {
+    document.getElementById("studenttableheader").innerHTML += `<th>${sunday.toLocaleDateString()}</th>`;
+});
+
 students.forEach((student) => {  
 
     studentdata = 
     `<tr id="${student.id}Data">
-        <td>${student.firstName}</td>`;
+        <td>${student.firstName} ${student.lastName}</td>
+        <td>${student.dateOfBirth}`;
 
     db.collection("students").doc(student.id).collection("Attendance").get().then((querySnapshot) => {
         let attendanceDates = [];
@@ -29,22 +36,57 @@ students.forEach((student) => {
             });    
         });
 
-
-        //Need to add code here to get array of Sunday Dates. Create a loop to test sortedAttendanceDates against sunday dates and display if true
-
         let sortedAttendanceDates = attendanceDates.sort((a, b) => new Date(b.id) < new Date(a.id) ? 1: -1);
 
-        sortedAttendanceDates.forEach((date) => {
-            document.getElementById(`${student.id}Data`).innerHTML += `<td>${new Date(date.id).toLocaleDateString()}<br> ${date.present ? "Present" : "Absent"}</td>`;
-        });
-    });
+        sundayDates.forEach((sunday) => {
+                sortedAttendanceDates.forEach((date) => {
+                console.log(new Date(date.id).toLocaleDateString());
+                console.log(new Date(sunday).toLocaleDateString());
+                if(new Date(date.id).toLocaleDateString() == new Date(sunday).toLocaleDateString()){
+                    document.getElementById(`${student.id}Data`).innerHTML += `<td>${date.present ? "Present" : "Absent"}</td>`;
+                }
+            });
 
-    
+            
+        });
+
+        
+        
+    });
 
     studentdata += '</tr>';
 
+    console.log(studentdata);
     document.getElementById('studenttable').innerHTML += studentdata;
     
 });
 
 }
+
+let getSundays = () =>
+{       
+    let sundays = []
+
+    let startDate = new Date(new Date().getFullYear(), 8, 1);
+    let endDate = new Date(new Date().getFullYear()+1, 6, 1);
+    
+    while(startDate.getDay() != 0)
+    {
+        startDate.setDate(startDate.getDate() + 1);
+        
+    }
+
+    sundays.push(new Date(startDate));
+
+    while(startDate < endDate)
+    {            
+        startDate.setDate(startDate.getDate()+7);
+        sundays.push(new Date(startDate));
+    }
+
+
+
+    return sundays;
+ 
+}
+
